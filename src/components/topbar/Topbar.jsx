71a770383fbfc522458abee56414328df3d304e3
Joey <jtroinacoding@gmail.com>
2022-02-08 00:38:03 -0500

    finished topbar, working on menu

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,28 @@
+import "./topbar.scss";
+import {Person, Mail} from "@material-ui/icons"
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+            <a href="#intro" className="logo">genuis.</a>
+            <div className="itemContainer">
+                <Person className="icon" />
+                <span>(718) 510-4452</span>
+            </div>
+            <div className="itemContainer">
+                <Mail className="icon" />
+                <span>jtroinacoding@gmail.com</span>
+            </div>
+        </div>
+        
+        <div className="right">
+            <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                <span className="line1"></span>
+                <span className="line2"></span>
+                <span className="line3"></span>
+            </div>
+        </div>
+      </div>
+  </div>;
+}
