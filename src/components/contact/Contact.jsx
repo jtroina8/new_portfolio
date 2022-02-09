ed9bc3c7fc70adeb87bd96bf3954114e2be6ebcf
Joey <jtroinacoding@gmail.com>
2022-02-08 19:16:48 -0500

    finished contact page
    added event handler and usestate

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
-  return <div className="contact" id="contact">
 
+  const [message, setMessage] = useState(false)
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  setMessage(true)
+}
+
+  return <div className="contact" id="contact">
+    <div className="left">
+      <img src="assets/shake.png" alt="" />
+    </div>
+    <div className="right">
+      <h2>Contact</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Email"/>
+        <textarea placeholder="Message"></textarea>
+        <button type="submit">Send</button>
+        {message && <span>Thanks! We'll talk soon. :)</span>}
+      </form>
+    </div>
   </div>;
 }
